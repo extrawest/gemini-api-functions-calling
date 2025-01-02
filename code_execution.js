9438d1b09c1e82f6552f751992125110585053e3
oleksandr.samoilenko <oleksandr.samoilenko@extrawest.com>
2025-01-02 17:17:16 +0200

    add code execution section

---
+++ code_execution.js
@@ -0,0 +1,66 @@
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import readline from 'readline';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({
+    model: 'gemini-1.5-pro',
+    tools: [
+        {
+            codeExecution: {},
+        },
+    ],
+});
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+async function askQuestion(query) {
+    return new Promise((resolve) => {
+        rl.question(query, (answer) => {
+            resolve(answer);
+        });
+    });
+}
+
+async function main() {
+    while (true) {
+        try {
+            const userQuestion1 = await askQuestion(
+                "Enter your condition (or type 'exit' to quit): "
+            );
+
+            if (userQuestion1.toLowerCase() === 'exit') {
+                console.log('Exiting the program. Goodbye!');
+                rl.close();
+                break;
+            }
+            let userQuestion2;
+            if (userQuestion1 !== '') {
+                userQuestion2 = await askQuestion(
+                    "Enter your question (or type 'exit' to quit): "
+                );
+                if (userQuestion2.toLowerCase() === 'exit') {
+                    console.log('Exiting the program. Goodbye!');
+                    rl.close();
+                    break;
+                }
+            }
+
+            if (userQuestion1 !== '' && userQuestion2 !== '') {
+                const result = await model.generateContent(
+                    userQuestion1 + userQuestion2
+                );
+                const response = result.response;
+            }
+        } catch (error) {
+            console.error('An error occurred:', error);
+        }
+    }
+}
+
+main();
